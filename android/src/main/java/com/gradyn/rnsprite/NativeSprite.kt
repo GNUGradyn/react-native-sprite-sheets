3c51d6a587dddadb082ffffffa2bb8de0c8396e4
Gradyn Wursten <me@gradyn.com>
2025-06-28 17:39:49 -0600

    refactor: update NativeSprite and SpriteSheetsPackage for improved view management

--- android/src/main/java/com/gradyn/rnsprite/NativeSprite.kt
+++ android/src/main/java/com/gradyn/rnsprite/NativeSprite.kt
@@ -1,18 +1,17 @@
 package com.margelo.nitro.rnsprite
 
-import android.graphics.BitmapFactory
-import android.graphics.Bitmap
-import android.widget.ImageView
-import androidx.appcompat.widget.AppCompatImageView
 import com.facebook.react.uimanager.ThemedReactContext
+import android.view.View
+
 
 class NativeSprite(context: ThemedReactContext) : HybridNativeSpriteSpec() {
+  override val view: View = View(context)
 
   override var srcX: Double = 0.0
   override var srcY: Double = 0.0
   override var srcW: Double = 0.0
   override var srcH: Double = 0.0  
-  override var width: Double = 0.0  
-  override var height: Double = 0.0  
-  override var assetID: Int = 0
+  override var width: Double? = 0.0  
+  override var height: Double? = 0.0  
+  override var assetID: Double = 0.0
 }
--- android/src/main/java/com/gradyn/rnsprite/SpriteSheetsPackage.kt
+++ android/src/main/java/com/gradyn/rnsprite/SpriteSheetsPackage.kt
@@ -4,6 +4,8 @@ import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.facebook.react.uimanager.ViewManager
+import com.margelo.nitro.rnsprite.views.HybridNativeSpriteManager
 
 class SpriteSheetsPackage : TurboReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
@@ -14,9 +16,9 @@ class SpriteSheetsPackage : TurboReactPackage() {
         return ReactModuleInfoProvider { HashMap() }
     }
 
-    override fun getViewManagers(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(NativeSpriteViewManager(reactContext))
-    }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<in Nothing, in Nothing>> {
+    return listOf(HybridNativeSpriteManager());
+  }
 
     companion object {
         init {
