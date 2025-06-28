31b5c1d920627d5fdafe0e414f2b6ac42e8878c1
Gradyn Wursten <me@gradyn.com>
2025-06-28 14:21:50 -0600

    fix build pipeline issues

--- android/src/main/java/com/gradyn/rnsprite/NativeSprite.kt
+++ android/src/main/java/com/gradyn/rnsprite/NativeSprite.kt
@@ -1,50 +1,18 @@
-package com.yourapp.spritesheets
+package com.margelo.nitro.rnsprite
 
 import android.graphics.BitmapFactory
 import android.graphics.Bitmap
 import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import com.facebook.react.uimanager.ThemedReactContext
-import com.margelo.nitro.HybridNativeSpriteSpec
 
-class NativeSprite(context: ThemedReactContext) : HybridNativeSpriteSpec(context) {
-  private var sheetBmp: Bitmap? = null
-
-  override val view = object : AppCompatImageView(context) {
-    override fun onDraw(canvas: Canvas) {
-      sheetBmp?.let { bmp ->
-        val src = Rect(
-          srcX.toInt(),
-          srcY.toInt(),
-          (srcX + srcW).toInt(),
-          (srcY + srcH).toInt()
-        )
-        val dst = Rect(0, 0, width, height)
-        canvas.drawBitmap(bmp, src, dst, null)
-      }
-    }
-  }
-
-  override var assetID: Double = 0.0
-    set(value) {
-      field = value
-      val resId = value.toInt()
-      sheetBmp = SheetCache.getOrLoad(view.context, resId)
-      view.invalidate()
-    }
+class NativeSprite(context: ThemedReactContext) : HybridNativeSpriteSpec() {
 
   override var srcX: Double = 0.0
   override var srcY: Double = 0.0
   override var srcW: Double = 0.0
   override var srcH: Double = 0.0  
-
-  private object SheetCache {
-    private val cache = mutableMapOf<Int, Bitmap>()
-
-    fun getOrLoad(ctx: Context, resId: Int): Bitmap {
-      return cache.getOrPut(resId) {
-        BitmapFactory.decodeResource(ctx.resources, resId)
-      }
-    }
-  }
+  override var width: Double = 0.0  
+  override var height: Double = 0.0  
+  override var assetID: Int = 0
 }
--- android/src/main/java/com/gradyn/rnsprite/SpriteSheetsPackage.kt
+++ android/src/main/java/com/gradyn/rnsprite/SpriteSheetsPackage.kt
@@ -1,4 +1,4 @@
-package com.spritesheets
+package com.margelo.nitro.rnsprite
 
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
